63908ad6dc5b99ab7e91313226aff2a6d48a5bdd
ruthra kumar <ruthra@erpnext.com>
2026-01-30 14:31:29 +0530

    refactor(test): remove redundant tearDown, tearDownClass and rollback

--- erpnext/maintenance/doctype/maintenance_schedule/test_maintenance_schedule.py
+++ erpnext/maintenance/doctype/maintenance_schedule/test_maintenance_schedule.py
@@ -141,8 +141,6 @@ class TestMaintenanceSchedule(ERPNextTestSuite):
 		serial_nos = get_serial_nos_from_schedule(mvi.item_name, ms.name)
 		self.assertEqual(serial_nos, ["TEST001", "TEST002"])
 
-		frappe.db.rollback()
-
 	def test_schedule_with_serials(self):
 		# Checks whether serials are automatically updated when changing in items table.
 		# Also checks if other fields trigger generate schdeule if changed in items table.
@@ -170,8 +168,6 @@ class TestMaintenanceSchedule(ERPNextTestSuite):
 		self.assertEqual(len(ms.schedules), 1)
 		ms.save()
 		self.assertEqual(len(ms.schedules), 2)
-
-		frappe.db.rollback()
 
 
 def make_serial_item_with_serial(self, item_code):
